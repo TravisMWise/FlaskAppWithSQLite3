7271af4446b92e262e3ff8eba8b01ef49b118670
Travis Wise <86330962+TravisMWise@users.noreply.github.com>
2023-07-16 01:00:28 -0400

    Add in functionality for updating the username and password for profile.py

--- flaskr/profile.py
+++ flaskr/profile.py
@@ -1,7 +1,8 @@
 from flask import (
-    Blueprint, render_template, request, g
+    Blueprint, render_template, request, g, flash, redirect, url_for
 )
 from flaskr.db import get_db
+from werkzeug.security import check_password_hash, generate_password_hash
 
 bp = Blueprint('profile', __name__, url_prefix='/profile')
 
@@ -21,9 +22,63 @@ def index():
 def update():
     return render_template('profile/update.html')
 
-@bp.route('/updateUsername')
+
+@bp.route('/updateUsername', methods=('GET', 'POST'))
 def updateUsername():
+    if request.method == 'POST':
+        db = get_db()
+        newUsername = request.form['username']
+        password = request.form['password']
+        
+        error = None
+        
+        user = db.execute(
+            f'SELECT * FROM user WHERE id = {g.user["id"]}'
+        ).fetchone()
+        if not check_password_hash(user['password'], password):
+            error = 'Incorrect password'
+        print(password, user['password'])
+        
+        if error is None:
+            db.execute(
+                'UPDATE user SET username = ? '
+                'WHERE id = ?',
+                (newUsername, g.user['id'])
+            )
+            db.commit()
+            return redirect(url_for('profile.index'))
+       
+        flash(error)
+
     return render_template('profile/updateUsername.html')
-@bp.route('/updatePassword')
+
+
+@bp.route('/updatePassword', methods=('GET', 'POST'))
 def updatePassword():
+    if request.method == 'POST':
+        oldPassword = request.form["oldPassword"]
+        newPassword = request.form["newPassword"]
+        confirmNewPassword = request.form["confirmNewPassword"]
+
+        db = get_db()
+        user = db.execute(
+            f'SELECT * FROM user WHERE id = {g.user["id"]}'
+        ).fetchone()
+        
+        error = None
+        if not check_password_hash(user["password"], oldPassword):
+            error = "Incorrect Password."
+        if newPassword != confirmNewPassword:
+            error = "Passwords do not match."
+        
+        if error is None:
+            db.execute(
+                'UPDATE user SET password = ? '
+                'WHERE id = ?',
+                (generate_password_hash(newPassword), g.user["id"])
+            )
+            db.commit()
+            return redirect(url_for('profile.index')) 
+
+
     return render_template('profile/updatePassword.html')
